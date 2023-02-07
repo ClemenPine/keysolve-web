fb6b69366f1d60ad48e0b763832b640c1a117ddb
ClemenPine <ClemenPine@pm.me>
2023-02-07 15:01:44 -0500

    mirror function

--- js/main.js
+++ js/main.js
@@ -10,3 +10,23 @@ window.theme = function(name) {
     const curr = document.getElementById('theme')
     curr.href = `themes/${name}.css`
 }
+
+window.mirror = function() {
+    const grid = document.getElementById('grid')
+    const keys = grid.children
+
+    let letters = []
+    for (const key of keys) {
+        letters.push(key.innerHTML)
+    }
+
+    for (let row=0; row < 3; row++) {
+        for (let col=0; col < 10; col++) {
+            const key = keys[(2-row)*10 + col]
+            const letter = letters.pop()
+
+            key.className = `cell center ${letter}`
+            key.innerHTML = letter
+        }
+    }
+}
